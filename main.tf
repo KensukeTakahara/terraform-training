6e40aad8452379a31a681f70dadf2d900d98f956
Kensuke Takahara <zawcjk@gmail.com>
2021-08-28 16:22:02 +0900

    separate data.tf.

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-data "aws_iam_policy_document" "allow_describe_regions" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ec2:DescribeRegions"]
-    resources = ["*"]
-  }
-}
-
 module "describe_regions_for_ec2" {
   source     = "./modules/iam_role"
   name       = "describe-region-for-ec2"
